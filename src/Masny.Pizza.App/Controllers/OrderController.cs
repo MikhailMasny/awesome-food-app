f55e5968c518cfcbbac33a969a2bbe82d553fcf9
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2021-02-28 19:45:59 +0300

    feat: add order list and mechanism for editing

--- src/Masny.Pizza.App/Controllers/OrderController.cs
+++ src/Masny.Pizza.App/Controllers/OrderController.cs
@@ -1,7 +1,9 @@
-﻿using Masny.Pizza.Data.Contexts;
+﻿using Masny.Pizza.App.ViewModels;
+using Masny.Pizza.Data.Contexts;
 using Masny.Pizza.Data.Models;
 using Masny.Pizza.Logic.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -127,6 +129,125 @@ namespace Masny.Pizza.App.Controllers
 
             return View(pdm);
         }
+
+        public async Task<IActionResult> List(int? status)
+        {
+            IQueryable<Order> orders = pizzaAppContext.Orders.AsNoTracking();
+            if (status != null && status != -1)
+            {
+                orders = orders.Where(p => p.Status == status);
+            }
+
+            var statusList = new List<StatusModel>
+            {
+                new StatusModel
+                {
+                    Id = 1,
+                    Name = "Nazvanie 1"
+                },
+                new StatusModel
+                {
+                    Id = 2,
+                    Name = "Nazvanie 2"
+                }
+            };
+
+            statusList.Insert(0, new StatusModel { Name = "Все", Id = -1 });
+
+            OrderListViewModel viewModel = new OrderListViewModel
+            {
+                Orders = await orders.ToListAsync(),
+                Statuses = new SelectList(statusList, "Id", "Name"),
+                CurrentStatus = status.HasValue ? status.Value : -1
+            };
+
+            return View(viewModel);
+
+
+
+
+
+
+            //var orders = await pizzaAppContext.Orders.AsNoTracking().ToListAsync();
+
+            ////var pdm =
+            ////    await pizzaAppContext.OrderProducts
+            ////        .Include(op => op.Product)
+            ////            .ThenInclude(p => p.ProductDetail)
+            ////        .AsNoTracking()
+            ////        .Where(pd => pd.OrderId == id)
+            ////        .ToListAsync();
+
+            ////_cartService.AddOrUpdate(1, HttpContext.User.Identity.Name, pdm);
+
+
+
+            //return View(orders);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var order = pizzaAppContext.Orders.AsNoTracking().FirstOrDefault(o => o.Id == id);
+
+
+
+            return View(new OrderEditViewModel
+            {
+                Id = order.Id,
+                Status = order.Status,
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(OrderEditViewModel model)
+        {
+            //model = model ?? throw new ArgumentNullException(nameof(model));
+
+            if (ModelState.IsValid)
+            {
+                var order = pizzaAppContext.Orders.FirstOrDefault(o => o.Id == model.Id);
+
+                order.Status = model.Status;
+
+                pizzaAppContext.SaveChanges();
+
+
+                return RedirectToAction("List", "Order");
+
+
+                //var user = new User
+                //{
+                //    Email = model.Email,
+                //    UserName = model.Username,
+                //};
+
+                //var result = await _userManager.CreateAsync(user, model.Password);
+                //if (result.Succeeded)
+                //{
+                //    await _signInManager.SignInAsync(user, false);
+
+                //    //_emailService.Send(model.Email, EmailResource.Subject, EmailResource.Message);
+
+                //    return RedirectToAction("Index", "Home");
+                //}
+
+                //foreach (var error in result.Errors)
+                //{
+                //    ModelState.AddModelError(string.Empty, error.Description);
+                //}
+            }
+
+            return View(model);
+        }
+
+    }
+
+    public class StatusModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
     }
 
     public class SimpleTestClass
---
+++ src/Masny.Pizza.App/ViewModels/OrderListViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Masny.Pizza.Data.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Masny.Pizza.App.ViewModels
+{
+    public class OrderListViewModel
+    {
+        public IEnumerable<Order> Orders { get; set; }
+
+        public SelectList Statuses { get; set; }
+
+        public int CurrentStatus { get; set; }
+    }
+}
